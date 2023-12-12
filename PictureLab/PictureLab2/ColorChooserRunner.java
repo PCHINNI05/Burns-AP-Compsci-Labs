4c6cb3fc725fb8f46045bf2381dbb159162f6681
PCHINNI05 <80406107+PCHINNI05@users.noreply.github.com>
2023-12-11 20:47:07 -0500

    Add files via upload

---
+++ PictureLab/PictureLab2/ColorChooserRunner.java
@@ -0,0 +1,10 @@
+package PictureLab2;
+import java.awt.Color;
+
+public class ColorChooserRunner {
+	  public static void main(String[] args)
+	  {
+	    Color pickedColor = ColorChooser.pickAColor();
+	    System.out.println(pickedColor);
+	  }
+}
